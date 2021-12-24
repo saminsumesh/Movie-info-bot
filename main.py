3197a7431c8de18bf4bcbaddf32efa98cdd0c450
PaulWalker <91833744+saminsumesh@users.noreply.github.com>
2021-12-24 19:20:12 +0530

    Just syntax

--- main.py
+++ main.py
@@ -9,7 +9,7 @@ API = "https://api.sumanjay.cf/watch/query="
 
 
 Bot = Client(
-"Movie info bot"
+"Movie info bot",
 bot_token = os.environ.get("BOT_TOKEN"),
 api_id = int(os.environ.get("API_ID")),
 api_hash = os.environ.get("API_HASH")
